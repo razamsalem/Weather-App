c68e5c8bca1ea130216d7f7a197430ea740150ca
Raz <amsalemraz10@gmail.com>
2023-12-20 14:13:00 +0200

    /F/WeatherIndex/Completion of the design phase

--- src/views/WeatherIndex.jsx
+++ src/views/WeatherIndex.jsx
@@ -1,8 +1,27 @@
+import searchIcon from '../assets/search.png'
+import clearIcon from '../assets/clear.png'
+import cloudIcon from '../assets/cloud.png'
+import drizzleIcon from '../assets/drizzle.png'
+import rainIcon from '../assets/rain.png'
+import snowIcon from '../assets/snow.png'
+import windIcon from '../assets/wind.png'
+import humidityIcon from '../assets/humidity.png'
+
 export function WeatherIndex() {
 
     return (
-        <div>
-            
+        <div className='weather-container'>
+            <div className="top-bar">
+                <input
+                    type="text"
+                    className="city-input"
+                    placeholder='Search'
+                />
+
+                <div className="search-icon">
+                    <img src={searchIcon} alt="Search icon" />
+                </div>
+            </div>
         </div>
-        )
+    )
 }
